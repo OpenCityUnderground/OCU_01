6a737f6f83941049653e2b0c6f96edb7727c8c1e
meady2 <martin.eady@jsi-group.com>
2013-08-19 21:04:34 -0500

    Added additional audio zones and audio fade in and out logic
    All rooms now have audio zones and the audio fade in/out program logic
    has been added. This program logic needs more work because it's not
    working 100% yet.

--- Level_01/Assets/AudioZone.cs
+++ Level_01/Assets/AudioZone.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class AudioZone : MonoBehaviour {
+	enum Fade {In, Out};
+	float fadeTime = 4.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +16,26 @@ public class AudioZone : MonoBehaviour {
 	// Enter room trigger
 	void OnTriggerEnter (Collider other){
 		audio.Play();
+		StartCoroutine(FadeAudio(fadeTime, Fade.In));
 	}
 	
 	// Exit room trigger
 	void OnTriggerExit (Collider other){
+		StartCoroutine(FadeAudio(fadeTime, Fade.Out));
 		audio.Stop();
+	}
+	
+	IEnumerator FadeAudio (float timer, Fade fadeType) {
+    	float start = fadeType == Fade.In? 0.0F : 3.0F;
+    	float end = fadeType == Fade.In? 3.0F : 0.0F;
+    	float i = 0.0F;
+    	float step = 1.0F/timer;
+ 
+    	while (i <= 3.0F) {
+        	i += step * Time.deltaTime;
+        	audio.volume = Mathf.Lerp(start, end, i);
+        	yield return new WaitForSeconds(step * Time.deltaTime);
+    	}	
 	}
 }
 

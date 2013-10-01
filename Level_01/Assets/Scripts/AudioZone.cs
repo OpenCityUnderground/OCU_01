18e87539697ff067903f90d747e31750e3b37d65
meady2 <martin.eady@jsi-group.com>
2013-10-01 12:23:50 -0500

    Fixed Audio Zone script
    Using Debug.log() command I set the audio zone script parameters for a 4
    second fade-in/fade-out when entering and exiting an audio zone.

--- Level_01/Assets/Scripts/AudioZone.cs
+++ Level_01/Assets/Scripts/AudioZone.cs
@@ -15,27 +15,34 @@ public class AudioZone : MonoBehaviour {
 
 	// Enter room trigger
 	void OnTriggerEnter (Collider other){
+		// Debug.Log("Trigger Enter");
 		audio.Play();
 		StartCoroutine(FadeAudio(fadeTime, Fade.In));
 	}
 	
 	// Exit room trigger
 	void OnTriggerExit (Collider other){
+		// Debug.Log("Trigger Exit");
 		StartCoroutine(FadeAudio(fadeTime, Fade.Out));
-		audio.Stop();
+		// audio.Stop();
 	}
 	
 	IEnumerator FadeAudio (float timer, Fade fadeType) {
-    	float start = fadeType == Fade.In? 0.0F : 3.0F;
-    	float end = fadeType == Fade.In? 3.0F : 0.0F;
+    	float start = fadeType == Fade.In? 0.0F : 1.0F;
+    	float end = fadeType == Fade.In? 1.0F : 0.0F;
     	float i = 0.0F;
     	float step = 1.0F/timer;
- 
-    	while (i <= 3.0F) {
+		
+ 		// Debug.Log("FadeAudio");
+		// Debug.Log("Out Step = " + step);
+		// Debug.Log("Out i = " + i);
+    	while (i <= 1.0F) {
+			// Debug.Log("Audio Volume i = " + i);
         	i += step * Time.deltaTime;
         	audio.volume = Mathf.Lerp(start, end, i);
+			// Debug.Log("Audio Volume = " + audio.volume);
         	yield return new WaitForSeconds(step * Time.deltaTime);
-    	}	
+    	}
+		// Debug.Log("Out of while loop");
 	}
 }
-
